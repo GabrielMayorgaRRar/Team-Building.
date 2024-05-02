6300e67dd5780eb3b5acd57f5fd9cdff26a4e46b
GabrielMayorgaRRar <142435908+GabrielMayorgaRRar@users.noreply.github.com>
2024-05-02 13:55:35 +0000

    Diccionario

--- diccionario.c
+++ diccionario.c
@@ -2,7 +2,20 @@
 
 void inicializar_diccionario(const char *nombre)
 {
-    printf("Diccionario de datos: %s\n", nombre);
+
+     long header = -1L;
+    FILE *ptr = fopen(nombre, "wb");
+    if (ptr == NULL)
+    {
+        fprintf(stderr, "FAILED TO OPEN THE DATA DICTIONARY");
+    }
+    else
+    {
+        fwrite(&header, sizeof(header), 1, ptr);
+    }
+    fclose(ptr);
+
+   
 }
 
 short abrir_diccionario_de_datos(const char *nombre)
@@ -23,21 +36,10 @@ short abrir_diccionario_de_datos(const char *nombre)
         status = EXIT_FAILURE;
     }
 
-    fclose(ptr);
-    return EXIT_SUCCESS;
+    return status;
 }
 
 void initializeDataDictionary(const char *filename)
 {
-    long header = -1L;
-    FILE *ptr = fopen(filename, "wb");
-    if (ptr == NULL)
-    {
-        fprintf(stderr, "FAILED TO OPEN THE DATA DICTIONARY");
-    }
-    else
-    {
-        fwrite(&header, sizeof(header), 1, ptr);
-    }
-    fclose(ptr);
+    
 }
